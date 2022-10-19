734b5cf8f948f5658e140e368014af31a755e8a0
Martin Fuchs <91662447+MFMentopolis@users.noreply.github.com>
2022-10-19 16:53:29 +0200

    Merge pull request #37 from openBackhaul:PrathibaJee/issue35 Implement Individual Services #39

--- server/controllers/IndividualServices.js
+++ server/controllers/IndividualServices.js
@@ -1,41 +1,58 @@
 'use strict';
 
-var utils = require('../utils/writer.js');
 var IndividualServices = require('../service/IndividualServicesService');
 var executionAndTraceService = require('onf-core-model-ap/applicationPattern/services/ExecutionAndTraceService');
+var responseBuilder = require('onf-core-model-ap/applicationPattern/rest/server/ResponseBuilder');
+var responseCodeEnum = require('onf-core-model-ap/applicationPattern/rest/server/ResponseCode');
+var restResponseHeader = require('onf-core-model-ap/applicationPattern/rest/server/ResponseHeader');
 
 module.exports.bequeathYourDataAndDie = function bequeathYourDataAndDie (req, res, next, body, user, originator, xCorrelator, traceIndicator, customerJourney) {
-  IndividualServices.bequeathYourDataAndDie(body, user, originator, xCorrelator, traceIndicator, customerJourney)
-    .then(function (response) {
-      utils.writeJson(res, response);
-      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, 200, req.body, responseBody);
+  let startTime = process.hrtime();
+  let responseCode = responseCodeEnum.code.OK;
+  IndividualServices.bequeathYourDataAndDie(body, user, originator, xCorrelator, traceIndicator, customerJourney, req.url)
+    .then(async function (response) {
+      let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
+      responseBuilder.buildResponse(res, responseCode, response, responseHeader);
+      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, response);
     })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, 500, req.body, responseBody);
+    .catch(async function (response) {
+      responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+      let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
+      responseBuilder.buildResponse(res, responseCode, response, responseHeader);
+      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, response);
     });
 };
 
 module.exports.listApplications = function listApplications (req, res, next, user, originator, xCorrelator, traceIndicator, customerJourney) {
-  IndividualServices.listApplications(user, originator, xCorrelator, traceIndicator, customerJourney)
-    .then(function (response) {
-      utils.writeJson(res, response);
-      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, 200, req.body, responseBody);
+  let startTime = process.hrtime();
+  let responseCode = responseCodeEnum.code.OK;
+  IndividualServices.listApplications(user, originator, xCorrelator, traceIndicator, customerJourney, req.url)
+    .then(async function (response) {
+      let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
+      responseBuilder.buildResponse(res, responseCode, response, responseHeader);
+      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, response);
     })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, 500, req.body, responseBody);
+    .catch(async function (response) {
+      responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+      let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
+      responseBuilder.buildResponse(res, responseCode, response, responseHeader);
+      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, response);
     });
 };
 
 module.exports.startApplicationInGenericRepresentation = function startApplicationInGenericRepresentation (req, res, next, user, originator, xCorrelator, traceIndicator, customerJourney) {
-  IndividualServices.startApplicationInGenericRepresentation(user, originator, xCorrelator, traceIndicator, customerJourney)
-    .then(function (response) {
-      utils.writeJson(res, response);
-      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, 200, req.body, responseBody);
+  let startTime = process.hrtime();
+  let responseCode = responseCodeEnum.code.OK;
+  IndividualServices.startApplicationInGenericRepresentation(user, originator, xCorrelator, traceIndicator, customerJourney, req.url)
+    .then(async function (response) {
+      let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
+      responseBuilder.buildResponse(res, responseCode, response, responseHeader);
+      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, response);
     })
-    .catch(function (response) {
-      utils.writeJson(res, response);
-      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, 500, req.body, responseBody);
+    .catch(async function (response) {
+      responseCode = responseCodeEnum.code.INTERNAL_SERVER_ERROR;
+      let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
+      responseBuilder.buildResponse(res, responseCode, response, responseHeader);
+      executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, response);
     });
 };
